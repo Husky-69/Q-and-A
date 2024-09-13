ea86a6844683c460f3455fa18f198959006fdc79
Husky-69 <95698104+Husky-69@users.noreply.github.com>
2024-09-13 18:23:06 +0300

    three.py

--- answer 1/three.py
+++ answer 1/three.py
@@ -1,1 +1,9 @@
 
+cur_age = 20
+
+years_to_add = 10 
+
+#future_age = current_age + a_few_years
+future_age = cur_age + years_to_add
+
+print(f"After {years_to_add},I will be {future_age} years old")
