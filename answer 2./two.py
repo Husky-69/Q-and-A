b4bfd09f34c5fe25ff8391bea4c6eb3423822558
Husky-69 <95698104+Husky-69@users.noreply.github.com>
2024-09-16 21:28:40 +0300

    two.py

--- answer 2./two.py
+++ answer 2./two.py
@@ -1,1 +1,12 @@
 
+num = int(input("Please enter a positive number!"))
+
+if num <= 0:
+    print ("Please enter positive number!")
+else:
+    print ("counting down!" )
+
+
+while num >= 1:
+    print (num)
+    num -= 1
